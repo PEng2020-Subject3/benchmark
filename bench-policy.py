21208cf683a932ee40acdbe124bbeb89620a3ccf
Fabian Stiehle <f.stiehle@gmail.com>
2020-07-23 00:18:29 +0200

    impl request body

--- bench-policy.py
+++ bench-policy.py
@@ -1,10 +1,13 @@
 import random
+import json
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(2, 4)
+    sensors = ["smart", "lambo", "angela merte"]
+    headers = {'content-type': 'text/plain'}
 
     @task
-    def index_page(self):
-        self.client.get("/function/nodeinfo?policy=test")
-
+    def function_call(self):
+        payload = {"sensor_ID": random.choice(self.sensors), "function": "genPerformanceScore"}
+        self.client.get("/function/performance-scores?policy=gdpr", headers=self.headers, data=json.dumps(payload))
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Run the Benchmark
 
-`locust -f bench-policy.py`
-`locust -f bench-default.py`
+`locust -f bench-policy.py --csv=policy --csv-full-history`
+`locust -f bench-default.py --csv=default --csv-full-history`
 
 Access the GUI at `http://127.0.0.1:8089/`
